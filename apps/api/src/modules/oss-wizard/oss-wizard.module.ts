451b02e451a869db52ff3f30f35de04285fb21f8
sin_ <93648188+yaxxsin@users.noreply.github.com>
2026-04-22 09:34:09 +0700

    [hotfix] fix: add missing StorageModule import to OssWizardModule (API crash)

--- apps/api/src/modules/oss-wizard/oss-wizard.module.ts
+++ apps/api/src/modules/oss-wizard/oss-wizard.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { OssWizardController } from './oss-wizard.controller';
 import { OssWizardService } from './oss-wizard.service';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [NotificationsModule],
+  imports: [NotificationsModule, StorageModule],
   controllers: [OssWizardController],
   providers: [OssWizardService],
   exports: [OssWizardService],
